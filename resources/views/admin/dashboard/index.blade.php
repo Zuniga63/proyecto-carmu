126021e32eb48def290297b370114a76d92c0c17
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2020-10-02 16:43:29 -0500

    create the dashboard index with layout let

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,16 @@
+@extends("theme/$theme/layout")
+
+@section('title', 'Dashboard')
+
+@section('contentTitle', "Dashboard")
+
+@section('contentBreadcrum')
+<ol class="breadcrumb float-sm-right">
+  <li class="breadcrumb-item"><a href="{{route('admin.admin')}}">Home</a></li>
+  <li class="breadcrumb-item active">Dashboard</li>
+</ol>
+@endsection
+
+@section('content')
+<p>Aquí va el contenido del dashboard</p>
+@endsection
