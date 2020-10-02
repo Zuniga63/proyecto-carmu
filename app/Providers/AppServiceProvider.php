d0617de899f302de547d30c9f7eecbc4f4a6fc3d
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2020-10-02 16:40:45 -0500

    add var theme in root for all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,32 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
+     * Con esto le estpi enviando a todas las vistas el
+     * nombre del tema que deben utilizar
      */
-    public function boot()
-    {
-        //
-    }
+    View::share('theme', 'lte');
+  }
 }
