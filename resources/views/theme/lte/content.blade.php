4fa146891725c765df69dc2abec359ae1449963f
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2020-10-04 14:36:57 -0500

    delete duplicate content header

--- resources/views/theme/lte/content.blade.php
+++ resources/views/theme/lte/content.blade.php
@@ -2,11 +2,6 @@
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
-    @yield('contentHeader')
-  </section>
-
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
