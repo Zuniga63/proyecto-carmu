9bcc82ac8b7288d34126b9fc1b7a0c6d9a10568f
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2020-10-17 10:29:49 -0500

    layout fixes

--- resources/views/theme/lte/sidebar.blade.php
+++ resources/views/theme/lte/sidebar.blade.php
@@ -2,9 +2,10 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="/" class="brand-link elevation-4">
-    <img src="{{asset("assets/$theme/dist/img/AdminLTELogo.png")}}" alt="AdminLTE Logo"
+    <img src="https://ui-avatars.com/api/?name=Tienda+Carmu&background=B80000&color=fff&font-size=0.6" alt="AdminLTE Logo"
       class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <span class="brand-text font-weight-light">Tienda <span class="text-bold">Carmú</span></span>
+    
   </a>
 
   <!-- Sidebar -->
