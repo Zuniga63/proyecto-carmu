3ab0096655e3733640691722e295f11d97e2d49b
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2020-10-31 15:02:37 -0500

    fixed bug with switches after remove some products

--- resources/views/admin/shop/product/table.blade.php
+++ resources/views/admin/shop/product/table.blade.php
@@ -16,83 +16,49 @@
       </thead>
       <tbody>
         @foreach ($products as $item)
-        <tr x-data="{
-          id:{{$item->id}}, 
-          name:'{{$item->name}}',
-          img: '{{$item->img ? 'Si' : 'No'}}',
-          price: {{$item->price}},
-          isNew: {{$item->is_new ? 'true' : 'false'}},
-          outstanding: {{$item->outstanding ? 'true' : 'false'}},
-          published: {{$item->published ? 'true' : 'false'}}
-          }"
-          x-cloak
-        >
-        {{-- <td x-text="id"></td> --}}
-        <td>
-          <img src="{{$item->img ? url('storage/' . $item->img) : url('storage/img/products/no-image-available.png')}}" width="100px" lazy>
-        </td>
-        <td x-text="name"></td>
-        <td x-text="formatCurrencyLite(price, 0)" class="text-right"></td>
-        <td>
-          <div class="form-group">
-            <div class="custom-control custom-switch">
-              <input 
-                x-bind:id="'switch-outstanding-' + id" 
-                type="checkbox" 
-                class="custom-control-input" 
-                x-model="outstanding"
-                x-on:change="$wire.changeState(id, 'outstanding', outstanding)"
-              >
-              <label x-bind:for="'switch-outstanding-' + id" class="custom-control-label"><i class="far fa-star"></i></label>
+        <tr>
+          <td>
+            <img src="{{$item->img ? url('storage/' . $item->img) : url('storage/img/products/no-image-available.png')}}"
+              width="100px" lazy>
+          </td>
+          <td>{{$item->name}}</td>
+          <td x-text="formatCurrencyLite({{$item->price}}, 0)" class="text-right"></td>
+          <td>
+            <div class="form-group">
+              <div class="custom-control custom-switch">
+                <input id="switch-outstanding-{{$item->id}}" type="checkbox" class="custom-control-input"
+                  {{$item->outstanding ? 'checked' : ''}}
+                  wire:change="changeState({{$item->id}}, 'outstanding', {{$item->outstanding ? 0 : 1}})">
+                <label for="switch-outstanding-{{$item->id}}" class="custom-control-label"><i class="far fa-star"></i></label>
+              </div>
+              <div class="custom-control custom-switch">
+                <input 
+                  id="switch-new-{{$item->id}}" 
+                  type="checkbox" class="custom-control-input" 
+                  {{$item->is_new ? 'checked' : ''}}
+                  wire:change="changeState({{$item->id}}, 'isNew', {{$item->is_new ? 0 : 1}})"
+                >
+                <label for="switch-new-{{$item->id}}" class="custom-control-label badge badge-danger">New</label>
+              </div>
+              <div class="custom-control custom-switch">
+                <input id="switch-public-{{$item->id}}" type="checkbox" class="custom-control-input" {{$item->published ? 'checked' : ''}}
+                wire:change="changeState({{$item->id}}, 'published', {{$item->published ? 0 : 1}})">
+                <label for="switch-public-{{$item->id}}" class="custom-control-label"><i class="fas fa-globe"></i></label>
+              </div>
             </div>
-            <div class="custom-control custom-switch">
-              <input 
-                x-bind:id="'switch-new-' + id" 
-                type="checkbox" 
-                class="custom-control-input" 
-                x-model="isNew"
-                x-on:change="$wire.changeState(id, 'isNew', isNew)"
-              >
-              <label x-bind:for="'switch-new-' + id" class="custom-control-label badge badge-danger">New</label>
+          </td>
+          <td>
+            <div class="btn-group-vertical">
+              <a href="#" class="btn btn-info block" data-toggle="tooltip" data-placement="top" title="Editar este registro"
+                wire:ignore wire:click="edit({{$item->id}})">
+                <i class="fas fa-pencil-alt"></i>
+              </a>
+              <a href="#" class="btn btn-danger" title="Eliminar producto" data-toggle="tooltip" data-placement="top"
+                x-on:click="showDeleteAlert(id, name)" wire:ignore>
+                <i class="fas fa-trash"></i>
+              </a>
             </div>
-            <div class="custom-control custom-switch">
-              <input 
-                x-bind:id="'switch-public-' + id" 
-                type="checkbox" 
-                class="custom-control-input" 
-                x-model="published"
-                x-on:change="$wire.changeState(id, 'published', published)"
-              >
-              <label x-bind:for="'switch-public-' + id" class="custom-control-label"><i class="fas fa-globe"></i></label>
-            </div>
-          </div>
-        </td>
-        <td>
-          <div class="btn-group-vertical">
-            <a 
-              href="#" 
-              class="btn btn-info block" 
-              data-toggle="tooltip" 
-              data-placement="top"
-              title="Editar este registro"
-              wire:ignore
-              wire:click="edit({{$item->id}})"
-            >
-              <i class="fas fa-pencil-alt"></i>
-            </a>
-            <a 
-              href="#" 
-              class="btn btn-danger" 
-              title="Eliminar producto"
-              data-toggle="tooltip" 
-              data-placement="top"
-              x-on:click="showDeleteAlert(id, name)"
-              wire:ignore
-            >
-              <i class="fas fa-trash"></i>
-            </a>
-          </div>
-        </td>
+          </td>
         </tr>
         @endforeach
       </tbody>
