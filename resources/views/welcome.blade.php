e232aca6435cea50b1bab97dc5fa4d0f8be26720
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2020-11-08 09:11:29 -0500

    added google analityc for production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,18 @@
 
   <link rel="stylesheet" href="./css/app.css">
 
+  @env('production')
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-16W3ED57KL"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-16W3ED57KL');
+  </script>
+  @endenv
+
 </head>
 
 <body class="antialised font-sans bg-gray-200 min-h-screen relative">
